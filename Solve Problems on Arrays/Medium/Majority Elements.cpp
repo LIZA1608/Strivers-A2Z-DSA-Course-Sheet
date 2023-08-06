c4338691c60beb4c5f82995ebd737fa6ae0f4f27
LIZA1608 <78140921+LIZA1608@users.noreply.github.com>
2023-08-06 12:30:33 +0530

    Update Majority Elements.cpp

--- Solve Problems on Arrays/Medium/Majority Elements.cpp
+++ Solve Problems on Arrays/Medium/Majority Elements.cpp
@@ -1,4 +1,50 @@
-
+//best->Moores Voting Algorithim->O(N)
+int majorityElement(vector<int> v) {
+	// Write your code here
+	int cnt=0;
+	int el;
+	int n=v.size();
+	for(int i=0;i<n;i++){
+		if(cnt==0){
+			cnt=1;
+			el=v[i];
+		}
+		else if(v[i]==el){
+			cnt++;
+		}
+		else{
+			cnt--;
+		}
+	}
+	int cnt1=0;
+	for(int i=0;i<n;i++){
+                if (v[i] == el) {
+                        cnt1++;
+                }
+        }
+                if(cnt1>n/2){
+			return el;
+		}
+		return -1;
+	
+}
+//hashing O(NLogN) S.C->O(N)
+#include<bits/stdc++.h>
+int majorityElement(vector<int> v) {
+	// Write your code here
+	map<int,int>mp;
+	int n=v.size();
+	for(int i=0;i<n;i++){
+		mp[v[i]]++;
+	}
+	for(auto it:mp){
+		if(it.second>n/2){
+			return it.first;
+		}
+	}
+	return -1;
+}
+//brute force->O(N^2)
 int majorityElement(vector<int> v) {
 	// Write your code here
 	int n=v.size();
