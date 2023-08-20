da940c7218825ab38bb3bc8e1fb76d3f86ba60f0
LIZA1608 <78140921+LIZA1608@users.noreply.github.com>
2023-08-20 16:06:22 +0530

    Create Cycle Detection in a Singly Linked List.cpp

---
+++ Learn LinkedList [Single LL, Double LL, Medium, Hard Problems]/Medium Problems of LL/Cycle Detection in a Singly Linked List.cpp
@@ -0,0 +1,15 @@
+// Time Complexity : O(N)
+//     Space Complexity : O(1)
+bool detectCycle(Node *head)
+{
+	Node*slow=head;
+    Node*fast=head;
+    while(fast!=NULL && fast->next!=NULL){
+        slow=slow->next;
+        fast=fast->next->next;
+        if(slow==fast){
+            return true;
+        }
+    }
+    return false;
+}
