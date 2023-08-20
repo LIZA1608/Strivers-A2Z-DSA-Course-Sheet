c735eed9046b8075f2d95ca2299a6e9f7a9a3e7d
LIZA1608 <78140921+LIZA1608@users.noreply.github.com>
2023-08-20 16:12:43 +0530

    Create Linked List Cycle II.cpp

---
+++ Learn LinkedList [Single LL, Double LL, Medium, Hard Problems]/Medium Problems of LL/Linked List Cycle II.cpp
@@ -0,0 +1,20 @@
+ // Time Complexity: O(N)
+ //    Space Complexity: O(1)
+Node *firstNode(Node *head)
+{
+  Node*slow=head;
+  Node*fast=head;
+  while(fast!=NULL && fast->next!=NULL){
+      slow=slow->next;
+      fast=fast->next->next;
+      if(slow==fast){
+          slow=head;
+          while(slow!=fast){
+              slow=slow->next;
+              fast=fast->next;
+          }
+          return slow;
+      }
+  }
+  return NULL;
+}
