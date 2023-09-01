bc1cdc7470a32d4dd76ba62a676509dab3607966
LIZA1608 <78140921+LIZA1608@users.noreply.github.com>
2023-09-01 13:49:38 +0530

    Update Climbing Stars.cpp

--- Dynamic Programming [Patterns and Problems]/1D DP/Climbing Stars.cpp
+++ Dynamic Programming [Patterns and Problems]/1D DP/Climbing Stars.cpp
@@ -1,3 +1,4 @@
+//tabulation with space optimization T.C->O(N),S.C->O(1)
 class Solution {
 public:
     int climbStairs(int n) {
@@ -12,3 +13,28 @@ public:
         return prev2;
     }
 };
+//tabulation T.C->O(N) ,S.C->O(N)
+int solve(int n,vector<int>&dp){
+    dp[0]=1;
+    dp[1]=1;
+    for(int i=2;i<=n;i++){
+        cout<<dp[i-1] <<" "<<dp[i-2];
+    dp[i]=dp[i-1]+dp[i-2];
+    }
+    return dp[n];
+}
+    int climbStairs(int n) {
+        vector<int>dp(n+1,-1);
+        return solve(n,dp);
+    }
+//MEMORIZATION T.C->O(N),S.C->O(N)
+int solve(int n,vector<int>&dp){
+    if(n<=1) return 1;
+    if(dp[n]!=-1) return dp[n];
+    dp[n]=solve(n-1,dp)+solve(n-2,dp);
+    return dp[n];
+}
+    int climbStairs(int n) {
+        vector<int>dp(n+1,-1);
+        return solve(n,dp);
+    }
