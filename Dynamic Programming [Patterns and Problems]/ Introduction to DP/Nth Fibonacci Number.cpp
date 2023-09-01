3c32694c53cc0d85a0d3677be37e64f8af898c80
LIZA1608 <78140921+LIZA1608@users.noreply.github.com>
2023-09-01 13:32:27 +0530

    Update Nth Fibonacci Number.cpp

--- Dynamic Programming [Patterns and Problems]/ Introduction to DP/Nth Fibonacci Number.cpp
+++ Dynamic Programming [Patterns and Problems]/ Introduction to DP/Nth Fibonacci Number.cpp
@@ -1,16 +1,54 @@
+//TABULATION WITH SPACE OPTIMIZATION  T.C->O(N)  S.C->O(1)
 #include<bits/stdc++.h>
 using namespace std;
+
+int fibo(int n){
+ int p1=0;
+ int p2=1;
+ for(int i=2;i<=n;i++){
+         int curr=p1+p2;
+         p1=p2;
+         p2=curr;
+ }
+ return p2;
+}
 int main()
 {
-        int n;
-        cin>>n;
-        int prev2=0;
-        int prev=1;
-        for(int i=2;i<=n;i++){
-                int curr=prev2+prev;
-                prev2=prev;
-                prev=curr;
-        }
-        cout<<prev;
-        return 0;
+      int n;
+      cin>>n;
+      cout<<fibo(n);
+}
+
+//TABULATION T.C->O(N),S.C->O(N)
+#include<bits/stdc++.h>
+using namespace std;
+int fibo(int n,vector<int>&dp){
+        dp[0]=0;dp[1]=1;
+       for(int i=2;i<=n;i++){
+               dp[i]=dp[i-1]+dp[i-2];
+       }
+        return dp[n];
+}
+int main()
+{
+      int n;
+      cin>>n;
+      vector<int>dp(n+1,-1);
+            cout<<fibo(n,dp);
+}
+//memorization T.C->O(N),S.C->O(N)+O(N)
+#include<bits/stdc++.h>
+using namespace std;
+int fibo(int n,vector<int>&dp){
+        if(n<=1) return n;
+        if(dp[n]!=-1) return dp[n];
+        dp[n]=fibo(n-1,dp)+fibo(n-2,dp);
+        return dp[n];
+}
+int main()
+{
+      int n;
+      cin>>n;
+      vector<int>dp(n+1,-1);
+            cout<<fibo(n,dp);
 }
